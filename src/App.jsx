8b34e2113221005cb11aa2ccbad258e58bf6d706
STCzao <Santicoronel0183@gmail.com>
2025-05-09 06:50:31 -0300

    Se termino el Navbar,trabajamos con los componentes Hero y Programs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import Navbar from './components/Navbar/Navbar'
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Programs from "./components/Programs/Programs";
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <Hero />
+      <div className="container">
+        <Programs />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
