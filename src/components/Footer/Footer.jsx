0a9f7da19fe0b068dd821a434536d093b0416256
STCzao <Santicoronel0183@gmail.com>
2025-05-26 10:56:30 -0300

    Cambio en los derechos del footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 export const Footer = () => {
   return (
     <div className="footer">
-      <p>© 2025 Santiago Coronel. Todos los derechos reservados.</p>
+      <p>© 2025 STCzao. Todos los derechos reservados.</p>
       <ul>
         <li>Términos de servicio</li>
         <li>Política de privacidad</li>
